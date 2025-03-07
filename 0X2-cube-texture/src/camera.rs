f35d123c648ce938f9f704feea18ea84eb3be89f
Phil Jeffes <philjeffes@hotmail.com>
2025-03-07 16:37:52 +0100

    intermediate checkin
    nearly working, the five face of the dice not mapping correctly

---
+++ 0X2-cube-texture/src/camera.rs
@@ -0,0 +1,132 @@
+use euclid::Transform3D;
+
+
+pub struct Camera {
+    pub projection: Transform3D<f32, (), ()>,
+    pub view: Transform3D<f32, (), ()>,
+    pub width: f32,
+    pub height: f32,
+}
+
+impl Camera {
+    pub fn new() -> Camera {
+        Camera {
+            projection: Transform3D::identity(),
+            view: Transform3D::identity(),
+            width: 0.0,
+            height: 0.0,
+        }
+    }
+    
+    pub fn move_camera_y(&mut self, y: f32) {
+        self.view.m42 = y;
+        // gloo_console::log!("y: {}", y);
+    }
+
+    pub fn move_camera_x(&mut self, x: f32) {
+        self.view.m41 = x;
+    }
+
+    pub fn setup(&mut self, width: f32, height: f32) {
+        self.width = width;
+        self.height = height;
+        
+        let field_of_view = std::f32::consts::PI / 4.0;
+        let aspect = width /height;
+        let z_near = 0.1;
+        let z_far = 100.0;
+        
+        // Not sure if we need to reset this but just in case
+        self.projection = Transform3D::identity();
+
+        // Maths copied from: https://github.com/toji/gl-matrix/blob/master/src/mat4.js\
+        // Apply perspective
+        let f = 1.0 / (field_of_view / 2.0).tan();
+        self.projection.m11 = f / aspect;
+        self.projection.m22 = f;
+        self.projection.m34 = -1.0;
+
+        let nf = 1.0 / (z_near - z_far);
+        self.projection.m33 = (z_far + z_near) * nf;
+        self.projection.m43 = (2.0 * z_far * z_near) * nf; 
+        
+        // Apply the look at
+        let epsilon: f32 = 0.000001;
+        let eye: [f32; 3] = [0.0, 0.0, 5.0];
+        let center: [f32; 3] = [0.0, 0.5, 0.0];
+        let up: [f32; 3] = [0.0, 1.0, 0.0];
+        
+        if eye[0] - center[0] < epsilon &&
+           eye[1] - center[1] < epsilon &&
+           eye[2] - center[2] < epsilon {
+            self.view = Transform3D::identity();
+            return;
+        }
+        let mut z = [eye[0] - center[0], eye[1] - center[1], eye[2] - center[2]];
+        let len = 1.0 / (z[0] * z[0] + z[1] * z[1] + z[2] * z[2]).sqrt();
+        z[0] *= len;
+        z[1] *= len;
+        z[2] *= len;
+
+        let mut x = [
+            up[1] * z[2] - up[2] * z[1], 
+            up[2] * z[0] - up[0] * z[2], 
+            up[0] * z[1] - up[1] * z[0]
+        ];
+        let len = (x[0] * x[0] + x[1] * x[1] + x[2] * x[2]).sqrt();
+        if len == 0.0 {
+            x[0] = 0.0;
+            x[1] = 0.0;
+            x[2] = 0.0;
+        } else {
+            let len = 1.0 / len;
+            x[0] *= len;
+            x[1] *= len;
+            x[2] *= len;
+        }
+
+        let mut y = [
+            z[1] * x[2] - z[2] * x[1], 
+            z[2] * x[0] - z[0] * x[2], 
+            z[0] * x[1] - z[1] * x[0]
+        ];
+        let len = (y[0] * y[0] + y[1] * y[1] + y[2] * y[2]).sqrt();
+        if len == 0.0 {
+            y[0] = 0.0;
+            y[1] = 0.0;
+            y[2] = 0.0;
+        } else {
+            let len = 1.0 / len;
+            y[0] *= len;
+            y[1] *= len;
+            y[2] *= len;
+        }
+        self.view.m11 = x[0];
+        self.view.m21 = x[1];
+        self.view.m31 = x[2];
+        self.view.m41 = -(x[0] * eye[0] + x[1] * eye[1] + x[2] * eye[2]);
+        
+        self.view.m12 = y[0];
+        self.view.m22 = y[1];
+        self.view.m32 = y[2];
+        self.view.m42 = -(y[0] * eye[0] + y[1] * eye[1] + y[2] * eye[2]);
+
+        self.view.m13 = z[0];
+        self.view.m23 = z[1];
+        self.view.m33 = z[2];
+        self.view.m43 = -(z[0] * eye[0] + z[1] * eye[1] + z[2] * eye[2]);
+
+        self.view.m14 = 0.0;
+        self.view.m24 = 0.0;
+        self.view.m34 = 0.0;
+        self.view.m44 = 1.0;       
+
+        // mat4.lookAt(this.viewMatrix, eye, center, up);
+    }
+
+    pub fn update_screen_dimensions(&mut self, width: f32, height: f32) {
+        if width != self.width || height != self.height {
+            self.setup(width, height);
+        }
+    }
+}
---
+++ 0X2-cube-texture/src/canvas_control.rs
@@ -0,0 +1,319 @@
+use web_sys::{window, HtmlCanvasElement, WebGlRenderingContext as GL};
+use yew::prelude::*;
+use gloo_net::http::Request;
+use wasm_bindgen_futures::*;
+use wasm_bindgen::{prelude::*, JsCast};
+use gloo_console::log;
+use gltf_json;
+
+use super::camera::Camera;
+use super::model::Model;
+
+pub struct CanvasControl {
+    callback: Closure<dyn FnMut()>,
+    canvas: Option<HtmlCanvasElement>,
+    gl: Option<GL>,
+    node_ref: NodeRef,
+    last_update: f64,
+    models: Vec::<Model>,
+    u_time: f32,
+    height: i32,
+    width: i32,
+    mouse_x: f32,
+    mouse_y: f32,
+    camera: Camera
+}
+
+pub enum CanvasControlMsg {
+    MouseDown((f64, f64)),
+    MouseUp((f64,f64)),
+    MouseMove((f64,f64)),
+    TouchStart((f64, f64)),
+    TouchEnd((f64, f64)),
+    TouchMove((f64, f64)),
+    ModelReceived(String, String),
+    Render,
+    Null
+}
+
+
+#[derive(Clone, Debug, PartialEq, Eq, Properties)]
+pub struct CanvasControlProps;
+
+
+impl Component for CanvasControl {
+    type Message = CanvasControlMsg;
+    type Properties = CanvasControlProps;
+
+    fn create(ctx: &Context<Self>) -> Self {
+        let comp_ctx = ctx.link().clone();
+        let callback =
+            Closure::wrap(Box::new(move || comp_ctx.send_message(CanvasControlMsg::Render)) as Box<dyn FnMut()>);
+
+        let width = window().unwrap().inner_width().unwrap().as_f64().unwrap();
+        let height = window().unwrap().inner_height().unwrap().as_f64().unwrap();
+
+        let comp_ctx = ctx.link().clone();
+        wasm_bindgen_futures::spawn_local(async move {
+            let filename = "assets/cube_tex_sq.gltf".to_string();
+            let response = Request::get(&filename.clone())
+                .header("Content-Type", "application/json")
+                .send()
+                .await;
+    
+            match response {
+                Ok(resp) => {
+                    let filecontent = match resp.text().await {
+                        Ok(val) => val,
+                        Err(err) => {
+                            gloo_console::log!("Error while getting gltf: {:?}", err.to_string());
+                            return;
+                        }
+                    };
+                    comp_ctx.send_message(CanvasControlMsg::ModelReceived(filename, filecontent));
+                }
+                Err(err) => {
+                    gloo_console::log!("Error while getting gltf: {:?}", err.to_string());
+                }
+            }
+        });
+
+        CanvasControl{
+            callback: callback,
+            canvas: None,
+            gl: None,
+            node_ref: NodeRef::default(),
+            last_update: instant::now(),
+            models: Vec::new(),
+            u_time: 0.0,
+            height: height as i32,
+            width: width as i32,
+            mouse_x: 0.85,
+            mouse_y: 0.85,
+            camera: Camera::new()
+        }
+    }
+
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool{
+        match msg {
+            CanvasControlMsg::MouseDown(evt) => {
+                self.mouse_x = evt.0 as f32 / self.width as f32;
+                self.mouse_y = evt.1 as f32 / self.height as f32;
+                true
+            },
+            CanvasControlMsg::MouseUp(_evt) => {
+                true
+            },
+            CanvasControlMsg::MouseMove(_evt) => {
+                true
+            },
+            CanvasControlMsg::TouchStart(evt) => {
+                self.mouse_x = evt.0 as f32 / self.width as f32;
+                self.mouse_y = evt.1 as f32 / self.height as f32;
+                true
+            },
+            CanvasControlMsg::TouchEnd(_evt) => {
+                true
+            },
+            CanvasControlMsg::TouchMove(evt) => {
+                self.mouse_x = evt.0 as f32 / self.width as f32;
+                self.mouse_y = evt.1 as f32 / self.height as f32;
+                true
+            },
+            CanvasControlMsg::Render => {
+           
+                // if !self.in_render_loop{
+                //     self.in_render_loop = true;
+                    // gloo_console::log!("Render called");
+                    self.render();
+                // }
+                true
+            },
+            CanvasControlMsg::ModelReceived(name, content) => {
+                gloo_console::log!("Model received! ", name.clone());
+                // gloo_console::log!("Model content: ", content.clone());
+                let gltf: gltf_json::Root = gltf_json::deserialize::from_reader(content.as_bytes()).unwrap();
+                // gloo_console::log!(gltf.accessors.len());
+                self.models.push(Model::new(name, gltf));
+                self.reload();
+
+                ctx.link().send_message(CanvasControlMsg::Render);
+                true
+            },
+            CanvasControlMsg::Null => {
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let onmousedown = ctx.link().callback(move |evt: MouseEvent| {
+            CanvasControlMsg::MouseDown((evt.page_x() as f64, evt.page_y() as f64))
+        });
+        let onmousemove = ctx.link().callback(move |evt: MouseEvent| {
+            CanvasControlMsg::MouseMove((evt.page_x() as f64, evt.page_y() as f64))
+        });
+        let onmouseup = ctx.link().callback(move |evt: MouseEvent| {
+            CanvasControlMsg::MouseUp((evt.page_x() as f64, evt.page_y() as f64))
+        });
+        let ontouchstart = ctx.link().callback(move |evt: TouchEvent | {
+            match evt.touches().get(0) {
+                Some(touch) => CanvasControlMsg::TouchStart((touch.page_x() as f64, touch.page_y() as f64)),
+                None => CanvasControlMsg::Null,
+            }
+        });
+        let ontouchend = ctx.link().callback(move |evt: TouchEvent | {
+            match evt.touches().get(0) {
+                Some(touch) => CanvasControlMsg::TouchEnd((touch.page_x() as f64, touch.page_y() as f64)),
+                None => CanvasControlMsg::Null,
+            }
+        });
+        let ontouchmove = ctx.link().callback(move |evt: TouchEvent | {
+            match evt.touches().get(0) {
+                Some(touch) => CanvasControlMsg::TouchMove((touch.page_x() as f64, touch.page_y() as f64)),
+                None => CanvasControlMsg::Null,
+            }
+        });
+
+        html! {
+            <div class="game_canvas">
+                <canvas id="canvas"
+                    style={"margin: 0px; width: 100vw; height: 100vh; left:0px; top:0px;"}
+                    onmousedown={onmousedown}
+                    onmousemove={onmousemove}
+                    onmouseup={onmouseup}
+                    ontouchstart={ontouchstart}
+                    ontouchend={ontouchend}
+                    ontouchmove={ontouchmove}
+                    ref={self.node_ref.clone()}
+                    tabindex = "1"
+                ></canvas>
+            </div>
+        }
+    }
+
+    fn rendered(&mut self, _ctx: &Context<Self>, _first_render: bool) {
+        // Grab context and other setup
+        let c = self.node_ref.cast::<HtmlCanvasElement>().unwrap();
+        let gl: GL = c
+            .get_context("webgl")
+            .unwrap()
+            .unwrap()
+            .dyn_into()
+            .unwrap();
+
+        gl.enable(GL::DEPTH_TEST);
+        gl.enable(GL::CULL_FACE);
+
+        c.set_width(self.width as u32);
+        c.set_height(self.height as u32);
+
+        self.canvas = Some(c);
+        self.gl = Some(gl);
+    }
+}
+
+impl CanvasControl {
+
+    fn canvas_update(&mut self) {
+        let now = instant::now();
+
+        if self.last_update >= now {
+            return;
+        }
+        let diff = now - self.last_update;
+
+        let delta = diff as f64 / 1000.0; // Frac of seconds
+        self.u_time += delta as f32;
+
+        self.width = window().unwrap().inner_width().unwrap().as_f64().unwrap() as i32;
+        self.height = window().unwrap().inner_height().unwrap().as_f64().unwrap() as i32;
+        
+        self.camera.update_screen_dimensions(self.width as f32, self.height as f32);
+
+        // Vibrate camera up and down
+        self.camera.move_camera_y(self.u_time.sin());
+        self.camera.move_camera_x(self.u_time.cos());
+
+
+        // Do updates using delta
+        self.last_update = now;
+
+        for model in self.models.iter_mut() {
+            model.update(delta as f32);
+        }
+    }
+
+    fn reload(&mut self) {
+        // Set up shaders and 
+        let gl: &GL = match &self.gl {
+            Some(gl)=> gl,
+            None => {
+                log!("ERROR Setting up scene without a proper gl context");
+                return;
+            }
+        };
+
+        self.camera.setup(self.width as f32,self.height as f32);
+
+        let _: &HtmlCanvasElement = match &self.canvas {
+            Some(canv) => canv,
+            None => return,
+        };
+
+        for model in self.models.iter_mut() {
+            model.setup_shader(gl, self.width as f32, self.height as f32);
+            model.load_textures(gl);
+            model.setup(gl);
+        }
+        
+        // // Store mouse location
+        // self.mouse_x_loc = gl.get_uniform_location(&shader_program, "mouse_x");
+        // gl.uniform1f(self.mouse_x_loc.as_ref() , 1.0);
+        // self.mouse_y_loc = gl.get_uniform_location(&shader_program, "mouse_y");
+        // gl.uniform1f(self.mouse_y_loc.as_ref() , 1.0);
+
+        // self.shader_program = Some(shader_program);
+    }
+
+    fn render(&mut self) {
+        self.canvas_update();
+        
+        let c = self.node_ref.cast::<HtmlCanvasElement>().unwrap();
+        c.set_width(self.width as u32);
+        c.set_height(self.height as u32);
+
+        let gl = self.gl.as_ref().expect("GL Context not initialized!");
+
+        gl.viewport(
+            0,
+            0,
+            self.width as i32,
+            self.height as i32,
+        );
+
+        gl.clear_color(0., 0.5, 0., 1.0);
+        gl.clear_depth(1.0);
+
+        // Clear the color buffer bit
+        gl.clear(GL::COLOR_BUFFER_BIT);
+       
+        // Update uniforms in the shaders - for now just the u_time (time since start in secs)
+        // gl.uniform1f(self.time_location.as_ref() , self.u_time as f32);
+
+        // Update the current mouse locations
+        // gl.uniform1f(self.mouse_x_loc.as_ref() , self.mouse_x);
+        // gl.uniform1f(self.mouse_y_loc.as_ref() , self.mouse_y);
+
+        for model in self.models.iter_mut() {
+            model.render(gl, self.u_time, &self.camera);
+        }
+
+        // gl.draw_arrays(GL::TRIANGLES, 0, self.tri_count);
+
+        window()
+            .unwrap()
+            .request_animation_frame(self.callback.as_ref().unchecked_ref())
+            .unwrap();
+    }
+}
